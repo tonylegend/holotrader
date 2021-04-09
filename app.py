2aa1466ff80c23eaebc55b0d163d64e74d08e256
Tony Chen <tonylegend@gmail.com>
2021-04-08 19:53:18 -0700

    1st commit for heroku

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
-from summa import summarizer
+# from summa import summarizer
 import streamlit as st
+import numpy as np
+import pandas as pd
 
 # Add title on the page
-st.title("Text summarization")
+st.title("Holo Trader")
 
 # Ask user for input text
-input_sent = st.text_area("Input Text", "", height=400)
+input_sent = st.text_area("Input Text", "", height=200)
 
 ratio = st.slider(
     "Summarization fraction", min_value=0.0, max_value=1.0, value=0.2, step=0.01
 )
 
-# Display named entities
-summarized_text = summarizer.summarize(
-    input_sent, ratio=ratio, language="english", split=True, scores=True
-)
-
-for sentence, score in summarized_text:
-    st.write(sentence)
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))

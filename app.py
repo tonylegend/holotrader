6ab739fb14656ac6d755f6c2d2fbe352fcdf71f8
Tony Chen <tonylegend@gmail.com>
2021-04-10 20:37:11 -0700

    1st commit for heroku

--- app.py
+++ app.py
@@ -1,20 +1,51 @@
-# from summa import summarizer
 import streamlit as st
-import numpy as np
-import pandas as pd
+# import asyncio
+# import nest_asyncio
+# from datetime import date, datetime, timedelta
+# import pytz
+# import numpy as np
+# import pandas as pd
+# import backend
+# from utils.config import config
 
 # Add title on the page
 st.title("Holo Trader")
-
-# Ask user for input text
-input_sent = st.text_area("Input Text", "", height=200)
-
-ratio = st.slider(
-    "Backtest Time", min_value=0.0, max_value=1.0, value=0.2, step=0.01
+st.markdown(
+    "## **Back-testing & Analysis tools**"
 )
 
-st.write("Here's our first attempt at using data to create a table:")
-st.write(pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-}))
+st.sidebar.header('User Input Parameters')
+
+# def user_input_features():
+#     ticker = st.sidebar.text_input("Ticker", 'TSLA')
+#     cash = st.sidebar.number_input("Cash", min_value=0, value=int(config.backtest.cash), format='%i')
+#     start_date = st.sidebar.date_input("Start Date", value=date.today())
+#     end_date = st.sidebar.date_input("End Date", value=date.today())
+#     return ticker, cash, start_date, end_date
+#
+# symbol, cash, start, end = user_input_features()
+# start = pd.to_datetime(start)
+# end = pd.to_datetime(end)
+#
+# module = st.sidebar.selectbox("Select Strategy Module", options=[name for name in config.backtest.modules.split(',')])
+#
+# new_bid = ""
+# est = pytz.timezone("America/New_York")
+#
+# strategy = st.sidebar.selectbox("Select a Strategy to Run", options=backend.test_list(module))
+# env = st.sidebar.selectbox("Select environment", ("PAPER", "BACKTEST", "PROD"))
+
+# @st.cache
+# def run_strategy():
+#     with st.spinner('Running Strategy...'):
+#         return st.success('Done!')
+
+
+# loop = asyncio.new_event_loop()
+# asyncio.set_event_loop(loop)
+# nest_asyncio.apply()
+# loop.run_until_complete(database.create_db_connection())
+
+# st.write("Strategy Parameters:")
+# if module and strategy and symbol:
+#     st.table(pd.DataFrame(backend.params_list(module, strategy, symbol)))
